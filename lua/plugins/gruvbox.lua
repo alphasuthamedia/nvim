d37a86ca88ee3bf6fae5081a55ac158eb2461928
alpha <alpha.sutha@ui.ac.id>
2026-02-17 21:07:03 +0700

    nviim

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,24 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  opts = {
+    terminal_colors = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    contrast = "medium", -- soft, medium, hard
+  },
+  config = function(_, opts)
+    require("gruvbox").setup(opts)
+    vim.cmd("colorscheme gruvbox")
+  end,
+}
+
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false, -- load saat startup
+  },
+}
+
