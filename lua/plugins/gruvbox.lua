28f4969c967cd78a87532248394f60c0ebaf6a4c
Alpha SM <alphasuthamedia@gmail.com>
2026-03-10 10:52:34 +0700

    changing to gruvbox tapi lebih oke

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,22 +1,10 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"mikesmithgh/gruvsquirrel.nvim",
+	lazy = false,
 	priority = 1000,
-	opts = {
-		terminal_colors = true,
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		contrast = "medium", -- soft, medium, hard
-	},
-	config = function(_, opts)
-		require("gruvbox").setup(opts)
-		vim.cmd("colorscheme gruvbox")
+	config = function()
+		require("gruvsquirrel").setup()
+		vim.cmd("colorscheme gruvsquirrel")
 	end,
+	opts = {},
 }
